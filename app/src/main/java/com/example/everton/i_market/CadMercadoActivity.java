260c756390d793eaaf8e880b13a276f9e7f9b4e9
DESKTOP-N9P5HQ9\Everton <everton.nasc43@gmail.com>
2018-05-13 10:57:42 -0300

    Design das telas de cadastro de produto e mercado

---
+++ app/src/main/java/com/example/everton/i_market/CadMercadoActivity.java
@@ -0,0 +1,13 @@
+package com.example.everton.i_market;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadMercadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_mercado);
+    }
+}
