eb88393b5b765f8407eaf2ba332ad82b2f37ca59
Everton <everton.nasc43@gmail.com>
2018-05-14 10:06:28 -0300

    Reenvio do layout das telas

--- app/src/main/java/com/example/everton/i_market/CadMercadoActivity.java
+++ app/src/main/java/com/example/everton/i_market/CadMercadoActivity.java
@@ -2,12 +2,33 @@ package com.example.everton.i_market;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CadMercadoActivity extends AppCompatActivity {
+    private EditText edtNick, edtEmail, edtSenha, edtMercado, edtTelefone, edtCNPJ, edtEndereco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_mercado);
+
+        edtNick = (EditText)findViewById(R.id.edtNick);
+        edtSenha = (EditText)findViewById(R.id.edtSenha);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
+        edtCNPJ = (EditText)findViewById(R.id.edtCnpj);
+        edtTelefone = (EditText)findViewById(R.id.edtTelefone);
+        edtEndereco = (EditText)findViewById(R.id.edtEndereco);
+        edtMercado = (EditText)findViewById(R.id.edtMercado);
     }
+
+    public void cadastrarMercado(View view){
+
+    }
+
+    public void cancelarCadastro(View view){
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/example/everton/i_market/CadProdutoActivity.java
+++ app/src/main/java/com/example/everton/i_market/CadProdutoActivity.java
@@ -2,12 +2,56 @@ package com.example.everton.i_market;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import static com.example.everton.i_market.EnumCategoriaProduto.enumCategoriaProdutoLista;
 
 public class CadProdutoActivity extends AppCompatActivity {
+    private EditText edtProduto, edtPreco;
+    private Spinner spinner;
+    private String[] listaCategoria = enumCategoriaProdutoLista();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_produto);
+
+        edtProduto = (EditText)findViewById(R.id.edtProduto);
+        edtPreco = (EditText)findViewById(R.id.edtPreco);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listaCategoria);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner = (Spinner)findViewById(R.id.spinnerCategoria);
+        spinner.setAdapter(adapter);
+
+        //Metodo para quando um elemento do Spinner é selecionado()
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+
+    public void cadastrarProduto(View view){
+
+    }
+
+    public void cancelarCadastro(View view){
+        finish();
+    }
+
+
 }
